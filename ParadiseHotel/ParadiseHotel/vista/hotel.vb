e3ad5b62c2798ea387af22c71760c7edf2938284
Gabriel Fernández <gabofer82@gmail.com>
2012-02-28 21:36:32 -0300

    Se hizo un metodo de comprobacion de entrada de datos en el panel alta de reservas, igualmente se conecto el boton de alta con su metodo asociado en la fachada
    Signed-off-by: Gabriel Fernández <gabofer82@gmail.com>

--- ParadiseHotel/ParadiseHotel/vista/hotel.vb
+++ ParadiseHotel/ParadiseHotel/vista/hotel.vb
@@ -60,4 +60,60 @@ Public Class Hotel
         Dim frm_nueva_habitación As Man_habitacion = New Man_habitacion()
         frm_nueva_habitación.ShowDialog()
     End Sub
+
+    Private Sub btn_reservar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_reservar.Click
+        If Me.panel_reservar_completo() Then
+            'Dominio.Fachada.altaReserva(Me.txt_id_cliente.Text, Me.lview_servicios.SelectedItems.Item, Me.dtp_checkin.Value, Me.dtp_checkout.Value)
+        End If
+    End Sub
+
+    '*****************CONTROL DE DATOS*****************
+
+    Private Function panel_reservar_completo() As Boolean
+        Dim campos_faltantes As ArrayList = New ArrayList
+        Dim hay_faltante = False
+        If Not Me.txt_id_cliente.Text.Contains("") Then
+            hay_faltante = True
+            campos_faltantes.Add("identificar al cliente")
+        End If
+        If Me.lview_servicios.SelectedItems.Count < 1 Then
+            hay_faltante = True
+            campos_faltantes.Add("seleccionar una habitación")
+        End If
+        If DateDiff(DateInterval.Day, Me.dtp_checkin.Value, Date.Now) < 1 Then
+            hay_faltante = True
+            campos_faltantes.Add("la fecha de ingreso no puede ser anterior a la fecha actual")
+        End If
+        If DateDiff(DateInterval.Day, Me.dtp_checkout.Value, Date.Now) < 1 Then
+            hay_faltante = True
+            campos_faltantes.Add("la fecha de salida no puede ser anterior a la fecha actual")
+        End If
+        If DateDiff(DateInterval.Day, Me.dtp_checkin.Value, Me.dtp_checkout.Value) < 1 Then
+            hay_faltante = True
+            campos_faltantes.Add("la fecha de ingreso no puede ser anterior a la fecha de salida")
+        End If
+        If DateDiff(DateInterval.Day, Me.dtp_checkout.Value, Me.dtp_checkin.Value) < 1 Then
+            hay_faltante = True
+            campos_faltantes.Add("la fecha de salida no puede ser anterior a la fecha de entrada")
+        End If
+
+        If hay_faltante Then
+            Dim mensaje = New String("")
+            For i = 0 To campos_faltantes.Count
+                If i < (campos_faltantes.Count - 2) Then
+                    mensaje = mensaje & campos_faltantes.Item(i) & ", "
+                Else
+                    If i = campos_faltantes.Count Then
+                        mensaje = mensaje & "."
+                    Else
+                        mensaje = mensaje & campos_faltantes.Item(i) & " y "
+                    End If
+                End If
+            Next
+            MsgBox("Cuidado: " & mensaje)
+            Return False
+        Else
+            Return True
+        End If
+    End Function
 End Class
